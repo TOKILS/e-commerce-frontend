b5fa7fa68814222d266a0148522bac1c22d8b332
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-10-19 22:14:15 +0300

    added check to homepage image to stop crashes

--- src/components/Products/ProductCard/ProductCard.js
+++ src/components/Products/ProductCard/ProductCard.js
@@ -1,8 +1,4 @@
-import {
-  FavoriteBorderOutlined,
-  SearchOutlined,
-  ShoppingCartOutlined,
-} from "@material-ui/icons";
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -16,126 +12,126 @@ import { updateCart } from "../../../store/cart/cart";
 import cookie from "react-cookies";
 
 const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.5s ease;
-  cursor: pointer;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5s ease;
+    cursor: pointer;
 `;
 
 const Container = styled.div`
-  flex: 1;
-  margin: 5px;
-  min-width: 280px;
-  height: 350px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5fbfd;
-  position: relative;
-  &:hover ${Info} {
-    opacity: 1;
-  }
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
+    &:hover ${Info} {
+        opacity: 1;
+    }
 `;
 
 const Circle = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-color: white;
-  position: absolute;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
 `;
 
 const Image = styled.img`
-  height: 75%;
-  z-index: 2;
+    height: 75%;
+    z-index: 2;
 `;
 
 const Icon = styled.div`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
-  transition: all 0.5s ease;
-  &:hover {
-    background-color: #e9f5f5;
-    transform: scale(1.1);
-  }
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    transition: all 0.5s ease;
+    &:hover {
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+    }
 `;
 
 const Product = ({ product, handleClick }) => {
-  const context = useContext(AuthContext);
-  const dispatch = useDispatch();
-  const addToCart = () => {
-    if (context.loggedIn) {
-      superagent
-        .post(`https://mid-project-01.herokuapp.com/api/v2/Cart`)
-        .send({
-          ProductID: product.id,
-          UserID: context.user.id,
-          ColorID: product.color[0].id,
-          SizeID: product.color[0].size[0].id,
-        })
-        .set("Authorization", "Bearer " + context.token)
-        .then((res) => {
-          dispatch(updateCart());
-          handleClick();
-        });
-    }
-  };
-  const addToWish = () => {
-    if (context.loggedIn) {
-      superagent
-        .post(`https://mid-project-01.herokuapp.com/api/v2/Wishlist`)
-        .send({
-          ProductID: product.id,
-          UserID: context.user.id,
-          ColorID: product.color[0].id,
-          SizeID: 1,
-        })
-        .set("Authorization", "Bearer " + context.token)
-        .then((res) => {
-          handleClick();
-        });
-    }
-  };
+    const context = useContext(AuthContext);
+    const dispatch = useDispatch();
+    const addToCart = () => {
+        if (context.loggedIn) {
+            superagent
+                .post(`https://mid-project-01.herokuapp.com/api/v2/Cart`)
+                .send({
+                    ProductID: product.id,
+                    UserID: context.user.id,
+                    ColorID: product.color[0].id,
+                    SizeID: product.color[0].size[0].id,
+                })
+                .set("Authorization", "Bearer " + context.token)
+                .then((res) => {
+                    dispatch(updateCart());
+                    handleClick();
+                });
+        }
+    };
+    const addToWish = () => {
+        if (context.loggedIn) {
+            superagent
+                .post(`https://mid-project-01.herokuapp.com/api/v2/Wishlist`)
+                .send({
+                    ProductID: product.id,
+                    UserID: context.user.id,
+                    ColorID: product.color[0].id,
+                    SizeID: 1,
+                })
+                .set("Authorization", "Bearer " + context.token)
+                .then((res) => {
+                    handleClick();
+                });
+        }
+    };
 
-  return (
-    <Container>
-      <Circle />
-      <Image src={product.color[0].image[0].Image} />
-      <Info>
-        <Icon>
-          <ShoppingCartOutlined onClick={addToCart} />
-        </Icon>
-        <Icon
-          onClick={() => {
-            dispatch(update(product));
-            localStorage.setItem("product", JSON.stringify(product));
-          }}
-        >
-          <Link exact to="/Product">
-            <SearchOutlined />
-          </Link>
-        </Icon>
-        <Icon>
-          <FavoriteBorderOutlined onClick={addToWish} />
-        </Icon>
-      </Info>
-    </Container>
-  );
+    return (
+        <Container>
+            <Circle />
+            <Image src={product?.color[0]?.image[0]?.Image ? product.color[0].image[0].Image : "https://i.imgur.com/odGVdde.png"} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined onClick={addToCart} />
+                </Icon>
+                <Icon
+                    onClick={() => {
+                        dispatch(update(product));
+                        localStorage.setItem("product", JSON.stringify(product));
+                    }}
+                >
+                    <Link exact to="/Product">
+                        <SearchOutlined />
+                    </Link>
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined onClick={addToWish} />
+                </Icon>
+            </Info>
+        </Container>
+    );
 };
 
 export default Product;
