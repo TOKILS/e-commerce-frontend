4ee9334d4332d9d0c0822fdf6181dedbd8897f58
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-10-19 18:13:11 +0300

    fix

--- src/components/dashBoard/subComponents/tabs/Support.jsx
+++ src/components/dashBoard/subComponents/tabs/Support.jsx
@@ -1,7 +1,12 @@
 export default function Support(props) {
     return (
         <>
-            <div className="support">Support</div>
+            <div className="support">
+                <h3>Support</h3>
+            <iframe style={{height:"600px", width:"100%"}} src="https://mid-project-01.herokuapp.com/admin" title="admin">
+            </iframe>
+            </div>
+     
         </>
     );
 }
