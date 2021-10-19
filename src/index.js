96fcb7e4ea59f2b3dfe2937cad1b30da5b0f8a15
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-10-19 18:49:50 +0300

    finish_aboutUs&filterhome page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from "redux";
